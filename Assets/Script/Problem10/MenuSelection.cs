90d5d6a56d7e6bf58f514b8db393dcd14fa550f8
Nezaras <rizkialamsy@gmail.com>
2021-11-21 23:46:19 +0700

    menu selection

---
+++ Assets/Script/Problem10/MenuSelection.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuSelection : MonoBehaviour
+{
+    public static bool canDestroy = true;
+    private void Awake()
+    {
+        if (canDestroy)
+        {
+            DontDestroyOnLoad(gameObject);
+            canDestroy = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene("Problem 10");
+            Time.timeScale = 1;
+        }
+    }
+
+    public void LoadScene(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
