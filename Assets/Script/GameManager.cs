59d97378eeb5e5770f68732490b7e83b53f8c1ed
Nezaras <rizkialamsy@gmail.com>
2021-11-21 21:32:08 +0700

    done

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public Text ScoreText;
+    private int score = 0;
+
+    public void IncreaseScore(int value)
+    {
+        ScoreText.text = $"Score {(score += value)}";
+    }
+}
---
+++ Assets/Script/coin.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class coin : MonoBehaviour
+{
+    private string _circleTag = "Circle";
+    private string _gameManagerTag = "manager";
+    private GameManager _gameManager;
+
+    private void Start()
+    {
+        GameObject targetObj = GameObject.FindGameObjectWithTag(_gameManagerTag);
+        _gameManager = targetObj.GetComponent<GameManager>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.CompareTag(_circleTag))
+        {
+            _gameManager.IncreaseScore(1);
+            gameObject.SetActive(false);
+        }
+    }
+}
